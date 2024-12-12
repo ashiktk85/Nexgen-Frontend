cacfbc90e01280a2bf4b3d96a0430207e800c7d1
Rony George <ronygeorgengit@gmail.com>
2024-12-12 10:55:18 +0530

    Forgot password component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import LoginPage from './pages/Login'
+import ForgotPassword from './pages/ForgotPassword'
+
 
 function App() {
 
   return (
     <>
-      <LoginPage />
+    <Router>
+      <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
+    </Router>
     </>
   )
 }
