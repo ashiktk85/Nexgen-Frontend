48471faef2dcd1871ce970a554bd0eb8ed4e4027
Dhinu C Philip <dhinucphilip1022001@gmail.com>
2024-12-28 19:27:31 +0530

    Employer route setted

---
+++ src/pages/Employer/Outlets/CompanyDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompanyDetails() {
+  return (
+    <div className="my-6 px-2">
+      <h1>Company Details</h1>
+    </div>
+  )
+}
+
+export default CompanyDetails
