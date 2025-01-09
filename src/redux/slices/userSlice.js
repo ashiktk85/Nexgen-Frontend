b931fcbad6e7f946fb45c0de11aa6c0d8b5edd02
AthulKrishna <athulkrishna9633141960@gmail.com>
2025-01-09 20:05:51 +0530

    Add store for user

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,25 +1,38 @@
 
 import { createSlice } from "@reduxjs/toolkit";
+import { userLoginAction } from "../actions/userAction";
 
 
 const initialState = {
-    userId: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    isBlocked: false
+  seekerInfo:{},
+  error: null
 }
 
 
 const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers: {},
-    extraReducers: {
+    reducers: {
+        logout: (state) => {
+            state.seekerInfo = {}
+            state.error = null
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(userLoginAction.fulfilled, (state, action) => {
+            if(action.payload){
+                state.seekerInfo = action.payload?.userData || {}
+                console.log('User data in store: ', state.seekerInfo)
+            }
+        })
+        .addCase(userLoginAction.rejected, (state, action) => {
+            state.error = action.payload || 'Login failed'
+        })
 
     }
 })
 
+export const {logout} = userSlice.actions
 
 export default userSlice.reducer
