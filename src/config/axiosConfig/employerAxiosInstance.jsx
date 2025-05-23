18ec68cc37d56fbcd33b643496e143a11cd6d190
Jyothis-Tency <jyothisgtency@gmail.com>
2025-05-23 11:58:17 +0530

    url

--- src/config/axiosConfig/employerAxiosInstance.jsx
+++ src/config/axiosConfig/employerAxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const env = import.meta.env;
 
 const employerAxiosInstance = axios.create({
-  baseURL: `http://localhost:3001/employer`,
+  baseURL: `https://api.techpath.in/employer`,
 
   withCredentials: true,
 });
